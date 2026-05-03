66558d2de1ecbf7e6f2ac08c624ee52d90d1763d
santosharron <santosharron2906@gmail.com>
2026-05-03 16:15:57 +0530

    Added Blue Green

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
+# The Main Live Page
 @app.route('/')
-def hello():
-    # This tells Flask to look inside the "templates" folder for this file
-    return render_template('index.html')
+def live_site():
+    return '''
+    <body style="background-color: #f4f4f9; text-align: center; font-family: Arial; padding-top: 50px;">
+        <h1>Hello, Dockerized World!</h1>
+        <h2>This is the live production traffic.</h2>
+    </body>
+    '''
+
+# The Blue Environment
+@app.route('/blue')
+def blue_env():
+    return '''
+    <body style="background-color: lightblue; text-align: center; font-family: Arial; padding-top: 50px;">
+        <h1>BLUE Environment</h1>
+        <h2>Version 1.0 - Currently Active</h2>
+    </body>
+    '''
+
+# The Green Environment
+@app.route('/green')
+def green_env():
+    return '''
+    <body style="background-color: lightgreen; text-align: center; font-family: Arial; padding-top: 50px;">
+        <h1>GREEN Environment</h1>
+        <h2>Version 2.0 - Testing / Standby</h2>
+    </body>
+    '''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
